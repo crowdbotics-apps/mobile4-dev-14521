30b6ed2577955447be3d3d3cfc89cf8ed46cdc47
Crowdbotics <team@crowdbotics.com>
2020-11-04 06:48:07 +0000

    add component hello4nov 22

---
+++ src/features/Hello4Nov22313802/screens/blank.js
@@ -0,0 +1,49 @@
+import React from "react"
+import {
+  View,
+  Image,
+  ImageBackground,
+  TouchableOpacity,
+  Text,
+  Button,
+  Switch,
+  TextInput,
+  StyleSheet
+} from "react-native"
+import DateTimePicker from "react-native-datepicker"
+import Icon from "react-native-vector-icons/FontAwesome"
+import Slider from "@react-native-community/slider"
+import { CheckBox } from "react-native-elements"
+import { SlideMenuIcon } from "../../../navigator/slideMenuIcon"
+
+export default class Blank extends React.Component {
+  static navigationOptions = ({ navigation }) => {
+    return {
+      headerLeft: <SlideMenuIcon navigationProps={navigation} />
+    }
+  }
+
+  state = { TextInput_3: "" }
+
+  render = () => (
+    <View>
+      <Button title="Press me!" onPress={() => alert("Pressed!")} />
+      <TextInput
+        placeholder="Number Input Placeholder"
+        keyboardType="numeric"
+        value={this.state.TextInput_3}
+        onChangeText={nextValue => this.setState({ TextInput_3: nextValue })}
+      />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginHorizontal: 16
+  },
+  View_1: {},
+  Button_2: {},
+  TextInput_3: {}
+})
